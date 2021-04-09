58df3aef66b6395dac9268eabb26515aecb8476b
John Shaks <johnshaks100100@gmail.com>
2021-04-09 08:35:08 +0100

    rersponsive .snapshot-box-content

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ $(document).ready(function() {
     // toggle menu_nav-bar script
     $('.menu-btn').click(function() {
         $('nav-bar .nav-links').toggleClass("active)");
+        $('nav-bar i').toggleClass("active)");
     });
 });
